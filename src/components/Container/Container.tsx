4ad3edf281ebc01933269a0818aeab8c808f4670
VictorTamayoBriones <victortamayo509@gmail.com>
2023-05-01 17:19:07 -0600

    feat: :lipstick: theme color

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.article`
+    width: 356px;
+    height: 540px;
+`;
---
+++ src/theme.tsx
@@ -0,0 +1,10 @@
+export const theme = {
+    bg: 'F5F6FA',
+    white: '#FFFFFF',
+    blue: '#1240E4',
+    red: '#FF5858',
+    green: '#30C75A',
+    blue_dark: '#0E172C',
+    grey_light: '#D6D6D6',
+    grey_dark: '#888888'
+}
