192fce16cad06184de10369c75dcc5e564181065
VictorTamayoBriones <victortamayo509@gmail.com>
2023-05-11 01:19:56 -0600

    chore: update readme

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Alert';
+export * from './Buttons/Button/Button';
+export * from './Buttons';
+export * from './CheckOption';
+export * from './Container';
+export * from './Main';
+export * from './ShowField';
